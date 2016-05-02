f653cb5405fb5d48384ff2f60a5c4fec7dafbe61
Carmen Long <carmenlong@Carmens-MacBook-Pro-2.local>
2016-05-01 19:40:08 -0500

    change betteralert to allow for customizeable ok btn text

--- better-alert.js
+++ better-alert.js
@@ -1,15 +1,15 @@
-exports.module = function betterAlert(text) {
+exports.module = function betterAlert(text, buttonText) {
   var alertDOM = document.createElement("div");
   document.body.appendChild(alertDOM);
   alertDOM.classList.add('alert');
   var contents = document.createElement("div");
   alertDOM.appendChild(contents);
   contents.classList.add('container');
-  contents.innerHTML = "<h2 class=\"text\">" + text + "</h2>" +  "<button class=\"btn-primary ok-btn\" id=\"js-disableAlert\" type=\"submit\">OK</button>";
+  contents.innerHTML = "<h2 class=\"text\">" + text + "</h2>" +  "<button class=\"btn-primary ok-btn\" id=\"js-disableAlert\" type=\"submit\">"buttonText"</button>";
   var disable = document.getElementById('js-disableAlert');
   function removeAlert() {
     disable.removeEventListener('click', removeAlert, false);
     document.body.removeChild(alertDOM);
   }
   disable.addEventListener('click', removeAlert, false);
-}
+};
